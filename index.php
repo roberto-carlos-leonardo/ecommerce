0b29f15eded0287bb734162ef98776bb861b8f8a
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-09-17 18:54:07 -0300

    criação page login

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+// rota
 $app->get('/', function() {
     
 	$page = new Page();
@@ -18,11 +21,48 @@ $app->get('/', function() {
 
 });
 
+// rota
 $app->get('/admin', function() {
     
+    User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+// rota 
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+// rota 
+$app->post('/admin/login', function() {
+    
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+
+	exit;
+
+});
+
+// rota 
+$app->get('/admin/logout', function() {
+    
+	User::logout();
+
+	header("Location: /admin/login");
+
+	exit;
 
 });
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,7 +9,7 @@ class Page {
 
 	private $tpl;
 	private $options = [];
-	private $defaults = [ "data"=>[] ];
+	private $defaults = [ "header"=>true, "footer"=>true, "data"=>[] ];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/" ){
 
@@ -27,7 +27,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -49,7 +49,7 @@ class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 }
