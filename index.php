77d102c2f65798ca879330bbed486d8c4a6743ec
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-09-24 19:59:22 -0300

    criado paginas de categorias

--- index.php
+++ index.php
@@ -335,6 +335,22 @@ $app->post('/admin/categories/:idcategory', function($idcategory) {
 
 });
 
+$app->get('/categories/:idcategory', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		"category"=>$category->getValues(),
+		"products"=>[]
+	]);
+
+});
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -28,6 +28,7 @@ class Category extends Model {
 
 		$this->setData($results[0]);
 
+		Category::updatefile();
 	}
 
 	public function get($idcategory){
@@ -51,6 +52,22 @@ class Category extends Model {
 			":idcategory"=>$this->getidcategory()
 		]);
 
+		Category::updatefile();
+
+	}
+
+	public static function updatefile(){
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", 
+			implode('', $html));
 	}
 
 }
