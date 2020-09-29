b166501c4ca0cb724aaa6596aab6d78ef660dabc
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-09-29 18:42:31 -0300

    criado paginas de produtos e preços

--- index.php
+++ index.php
@@ -4,15 +4,12 @@ session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Model\User;
-use \Hcode\Model\Category;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
--- site.php
+++ site.php
@@ -2,13 +2,18 @@
 
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 // rota
 $app->get('/', function() {
     
+    $products = Product::listAll();
+
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products"=>Product::checkList($products)
+	]);
 
 });
 
