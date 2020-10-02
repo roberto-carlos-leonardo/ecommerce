5f4432667dac7a4eecd73b5286a5153d854590bf
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-10-02 20:53:06 -0300

    criado carrinho produtos aula 117

--- site.php
+++ site.php
@@ -54,7 +54,7 @@ $app->get('/products/:desurl', function($desurl) {
 
 	$product = new Product();
 
-	$product-> getFromURL($desurl);
+	$product->getFromURL($desurl);
 
 	$page = new Page();
 
@@ -72,7 +72,67 @@ $app->get('/cart', function() {
 
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		"cart"=>$cart->getValues(),
+		"products"=>$cart->getProducts()
+
+	]);
+
+});
+
+$app->get('/cart/:idproduct/add', function($idproduct) {
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i = 0; $i < $qtd; $i++){
+
+		$cart->addProduct($product);
+
+	}
+
+
+	header("Location: /cart");
+
+	exit;
+
+});
+
+$app->get('/cart/:idproduct/minus', function($idproduct) {
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+
+	exit;
+
+});
+
+$app->get('/cart/:idproduct/remove', function($idproduct) {
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+
+	exit;
 
 });
 
