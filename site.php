e87c80f6bdcdba0cf9d393126a406664f888015b
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-10-09 21:15:32 -0300

    criado meus pedidos aula 125

--- site.php
+++ site.php
@@ -530,7 +530,6 @@ $app->post('/profile', function() {
 
 });
 
-
 $app->get("/order/:idorder", function($idorder){
 
 	User::verifyLogin(false);
@@ -617,6 +616,44 @@ $app->get("/boleto/:idorder", function($idorder){
 
 	require_once($path . "funcoes_itau.php");
 	require_once($path . "layout_itau.php");
+
+});
+
+$app->get("/profile/orders", function(){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders", [
+		'orders'=>$user->getOrders()
+	]);
+
+});
+
+$app->get("/profile/orders/:idorder", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = new Cart();
+
+	$cart->get((int)$order->getidcart());
+
+	$cart->getCalculateTotal();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders-detail", [
+		'order'=>$order->getValues(),
+		"cart"=>$cart->getValues(),
+		"products"=>$cart->getProducts()
+	]);
 
 });
 
