9f0906701d5e5e97786e6a1e3646af942d9e6138
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-10-13 21:24:16 -0300

    criado admin categorias search and pagination aula 129

--- admin-categories.php
+++ admin-categories.php
@@ -8,13 +8,40 @@ use \Hcode\Model\Product;
 $app->get('/admin/categories', function() {
 
 	User::verifyLogin();
-    
-    $categories = Category::listAll();
+
+	$search = (isset($_GET["search"])) ? $_GET["search"] : "";
+
+	$page = (isset($_GET["page"])) ? $_GET["page"] : 1;
+
+	if ($search != ""){
+
+		$pagination = Category::getPageSearch($search, $page);		
+	
+	} else {
+
+		$pagination = Category::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination["pages"]; $x++){
+
+		array_push($pages, [
+			"href"=>"/admin/users?".http_build_query([
+				"page"=>$x + 1,
+			]),
+			"text"=>$x + 1
+		]);
+
+	}
 
 	$page = new PageAdmin();
 
 	$page->setTpl("categories", [
-			"categories"=>$categories
+		"categories"=>$pagination["data"],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
 
 
